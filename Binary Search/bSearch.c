e326fa1e294ca060c9e1ee498d2343fb109b3999
Ahmed Samy Kamal <59089861+ahmedsamy207@users.noreply.github.com>
2021-08-30 03:59:04 +0200

    Add files via upload

---
+++ Binary Search/bSearch.c
@@ -0,0 +1,103 @@
+/* Author: Ahmed Samy Kamal*/
+/*- INCLUDES
+----------------------------------------------*/
+#include "bSearch.h"
+
+/*- GLOBAL EXTERN VARIABLES
+-------------------------------*/
+/* error state */
+EN_Error_t E_State = E_NOK;
+
+/*- LOCAL FUNCTIONS IMPLEMENTATION
+------------------------*/
+/************************************************************************************
+* Parameters (in): array to be searched, size of array
+* Parameters (out): Error State
+* Return value: EN_Error_t
+* Description: sorts a given array using insertion sort algorithm
+************************************************************************************/
+/* Function to sort an array using insertion sort*/
+EN_Error_t insertionSort(uint8_t* au8_arr, uint8_t u8_NumberOfElements)
+{
+    uint8_t u8_LoopCounter = Initial_Value;
+    uint8_t u8_HoleIndex = Initial_Value;
+    uint8_t u8_tempValue = Initial_Value;
+
+    for(u8_LoopCounter=1; u8_LoopCounter < u8_NumberOfElements-1; u8_LoopCounter++)
+    {
+        u8_HoleIndex = u8_LoopCounter;
+        u8_tempValue = au8_arr[u8_LoopCounter];
+        while(u8_HoleIndex > 0 && au8_arr[u8_HoleIndex-1]>u8_tempValue)
+        {
+            au8_arr[u8_HoleIndex] = au8_arr[u8_HoleIndex-1];
+            u8_HoleIndex--;
+        }
+        au8_arr[u8_HoleIndex] = u8_tempValue;
+    }
+    return E_OK;
+}
+
+/************************************************************************************
+* Parameters (in): array to be searched, size of array
+* Parameters (out): Array State
+* Return value: uint8_t
+* Description: checks if a given array is sorted or not
+************************************************************************************/
+uint8_t arraySortedOrNot(uint8_t* au8_arr, uint8_t u8_NumberOfElements)
+{
+    // Array has one or no element
+    if (u8_NumberOfElements == 0 || u8_NumberOfElements == 1)
+        return Arr_Sorted;
+
+    uint8_t u8_LoopCounter = Initial_Value;
+    for (u8_LoopCounter = 1; u8_LoopCounter < u8_NumberOfElements; u8_LoopCounter++)
+    {
+        // Unsorted pair found
+        if (au8_arr[u8_LoopCounter - 1] > au8_arr[u8_LoopCounter])
+        {
+            return Arr_Not_Sorted;
+        }
+
+    }
+    // No unsorted pair found
+    return Arr_Sorted;
+}
+
+/************************************************************************************
+* Parameters (in): array to be searched, size of array, number to be searched for
+* Parameters (out): Number
+* Return value: uint8_t
+* Description: searches for a number in an array
+************************************************************************************/
+uint8_t binarySearch(uint8_t* au8_arr, uint8_t u8_NumberOfElements, uint8_t u8_Number)
+{
+    if(arraySortedOrNot(au8_arr,u8_NumberOfElements)== Arr_Not_Sorted)
+    {
+        insertionSort(au8_arr,u8_NumberOfElements);
+    }
+   uint8_t u8_Start = Initial_Value;
+   uint8_t u8_End = u8_NumberOfElements-1;
+   uint8_t u8_Mid = Initial_Value;
+
+   while(u8_Start <= u8_End)
+   {
+       u8_Mid = u8_Start + ((u8_End-u8_Start)/2);
+
+       if(au8_arr[u8_Mid] == u8_Number)
+       {
+           //printf("\nNumber ( %d ) was found\n", u8_Number);
+           return au8_arr[u8_Mid];
+       }
+       else if(u8_Number < au8_arr[u8_Mid])
+       {
+           u8_End = u8_Mid-1;
+       }
+       else
+       {
+           u8_Start = u8_Mid+1;
+       }
+   }
+
+        printf("\nNumber ( %d ) was not found\n", u8_Number);
+       return Number_Not_Found;
+   }
---
+++ Binary Search/main.c
@@ -0,0 +1,11 @@
+/* Author: Ahmed Samy Kamal*/
+#include "bSearch.h"
+
+int main()
+{
+    uint8_t arr[15] = {7,8,3,10,44,48,78,12,17,26,11,21,55,57,68};
+    binarySearch(arr,15,21);
+
+    return 0;
+}
+
